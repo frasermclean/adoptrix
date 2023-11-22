94f4745dfea04f5a2ca260be2c346d4282f045ce
Fraser McLean <dev@frasermclean.com>
2023-11-22 09:12:26 +0800

    Added hashing utilities and tests

---
+++ src/Adoptrix.Application/Utilities/HashingUtilities.cs
@@ -0,0 +1,20 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace Adoptrix.Application.Utilities;
+
+public static class HashingUtilities
+{
+    /// <summary>
+    /// Computes a hash from the given values.
+    /// </summary>
+    /// <param name="values">The string values to derive the hash from.</param>
+    /// <returns>The computed hash string in Base64 format.</returns>
+    public static string ComputeHash(params string[] values)
+    {
+        var joinedValues = string.Join("", values);
+        var hash = MD5.HashData(Encoding.UTF8.GetBytes(joinedValues));
+
+        return Convert.ToBase64String(hash);
+    }
+}
---
+++ tests/Adoptrix.Application.Tests/Utilities/HashingUtilitiesTests.cs
@@ -0,0 +1,19 @@
+﻿using Adoptrix.Application.Utilities;
+
+namespace Adoptrix.Application.Tests.Utilities;
+
+public class HashingUtilitiesTests
+{
+    [Fact]
+    public void ComputeHash_GivenValues_ReturnsHash()
+    {
+        // arrange
+        var values = new[] { "foo", "bar" };
+
+        // act
+        var hash = HashingUtilities.ComputeHash(values);
+
+        // assert
+        hash.Should().NotBeNullOrEmpty();
+    }
+}
