62646eb3296f75b2539870a03b8d01a1674683d9
Fraser McLean <dev@frasermclean.com>
2023-11-23 20:16:03 +0800

    Added animals component

---
+++ frontend/src/app/animals/animals.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-animals',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './animals.component.html',
+  styleUrl: './animals.component.scss'
+})
+export class AnimalsComponent {
+
+}
--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,4 +1,8 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { AnimalsComponent } from './animals/animals.component';
 
-export const routes: Routes = [{ path: '', component: HomeComponent }];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'animals', component: AnimalsComponent },
+];
