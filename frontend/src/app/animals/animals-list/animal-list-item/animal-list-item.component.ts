74833356696c19ebc7414193ac62fde75ee5f0eb
Fraser McLean <dev@frasermclean.com>
2023-11-24 16:49:07 +0800

    Improved appearance of animal list item

--- frontend/src/app/animals/animals-list/animal-list-item/animal-list-item.component.ts
+++ frontend/src/app/animals/animals-list/animal-list-item/animal-list-item.component.ts
@@ -3,13 +3,20 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
 
 import { AnimalSearchResult } from '../../../models/api-responses';
 
 @Component({
   selector: 'app-animal-list-item',
   standalone: true,
-  imports: [CommonModule, MatCardModule, MatButtonModule, RouterModule],
+  imports: [
+    CommonModule,
+    RouterModule,
+    MatCardModule,
+    MatButtonModule,
+    MatDividerModule,
+  ],
   templateUrl: './animal-list-item.component.html',
   styleUrl: './animal-list-item.component.scss',
 })
