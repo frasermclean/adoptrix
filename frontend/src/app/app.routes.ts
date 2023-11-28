b41907bbe5d65969b3a034d8084978b885b8678d
Fraser McLean <dev@frasermclean.com>
2023-11-28 13:33:01 +0800

    Updated home component route

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,5 +1,5 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
+import { HomeComponent } from './core/home/home.component';
 import { AnimalsListComponent } from './animals/animals-list/animals-list.component';
 import { AnimalViewComponent } from './animals/animal-view/animal-view.component';
 
