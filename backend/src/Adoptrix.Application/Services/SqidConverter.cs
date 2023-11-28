2b0599a3ffa3d99df05091bc88157e3df2b0b419
Fraser McLean <dev@frasermclean.com>
2023-11-28 14:55:34 +0800

    Added SqidConverter service with tests

---
+++ backend/src/Adoptrix.Application/Services/SqidConverter.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Options;
+using Sqids;
+
+namespace Adoptrix.Application.Services;
+
+public interface ISqidConverter
+{
+    int CovertToInt(string sqid);
+    string ConvertToSqid(int id);
+}
+
+public class SqidConverter(IOptions<SqidsOptions> options)
+    : ISqidConverter
+{
+    private readonly SqidsEncoder<int> encoder = new(options.Value);
+
+    public int CovertToInt(string sqid)
+    {
+        return encoder.Decode(sqid)[0];
+    }
+
+    public string ConvertToSqid(int id)
+    {
+        return encoder.Encode(id);
+    }
+}
---
+++ backend/tests/Adoptrix.Application.Tests/Services/SqidConverterTests.cs
@@ -0,0 +1,47 @@
+﻿using Adoptrix.Application.Services;
+using Microsoft.Extensions.Options;
+using Sqids;
+
+namespace Adoptrix.Application.Tests.Services;
+
+public class SqidConverterTests
+{
+    private readonly ISqidConverter sqidConverter;
+
+    public SqidConverterTests()
+    {
+        var options = new SqidsOptions
+        {
+            Alphabet = "abcdefghijklmnopqrstuvwxyz0123456789",
+            MinLength = 5,
+        };
+
+        sqidConverter = new SqidConverter(Options.Create(options));
+    }
+
+    [Theory]
+    [InlineData(1, "52upd")]
+    [InlineData(69, "1k1p4")]
+    [InlineData(420, "nkoim")]
+    public void ConvertToSqid_Should_Return_ExpectedResult(int valueToConvert, string expectedResult)
+    {
+        // act
+        var result = sqidConverter.ConvertToSqid(valueToConvert);
+
+        // assert
+        result.Should().Be(expectedResult);
+    }
+
+    [Theory]
+    [InlineData("52upd", 1)]
+    [InlineData("1k1p4", 69)]
+    [InlineData("nkoim", 420)]
+    public void ConvertToInt_Should_Return_ExpectedResult(string valueToConvert, int expectedResult)
+    {
+        // act
+        var result = sqidConverter.CovertToInt(valueToConvert);
+
+        // assert
+        result.Should().Be(expectedResult);
+    }
+}
