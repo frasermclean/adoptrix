ea418bdd7acd9f42c04eaa6725b25017f2c4d20c
Fraser McLean <dev@frasermclean.com>
2023-11-29 21:58:29 +0800

    Refactored components using NGXS store

--- frontend/src/app/animals/animals.routes.ts
+++ frontend/src/app/animals/animals.routes.ts
@@ -1,8 +1,19 @@
 import { Route } from '@angular/router';
+import { importProvidersFrom } from '@angular/core';
+import { NgxsModule } from '@ngxs/store';
+
 import { AnimalsListComponent } from './animals-list/animals-list.component';
 import { AnimalViewComponent } from './animal-view/animal-view.component';
+import { AnimalsState } from './animals.state';
 
 export default [
-  { path: '', component: AnimalsListComponent },
-  { path: ':animalId', component: AnimalViewComponent },
+  {
+    path: '',
+    pathMatch: 'prefix',
+    children: [
+      { path: '', component: AnimalsListComponent },
+      { path: ':animalId', component: AnimalViewComponent },
+    ],
+    providers: [importProvidersFrom([NgxsModule.forFeature([AnimalsState])])],
+  },
 ] satisfies Route[];
--- frontend/src/app/services/animals.service.ts
+++ frontend/src/app/services/animals.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
 import { HttpClient, HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 import { Animal } from '@models/animal.model';
 import { Species } from '@models/species.enum';
@@ -13,19 +14,24 @@ export class AnimalsService {
 
   constructor(private httpClient: HttpClient) {}
 
-  public searchAnimals({ species, name }: { species?: Species; name?: string; } = {}) {
-    let params = new HttpParams();
-    if (species) {
-      params = params.set('species', species);
+  public searchAnimals(params: SearchAnimalsParams = {}): Observable<Animal[]> {
+    let httpParams = new HttpParams();
+    if (params.species) {
+      httpParams = httpParams.set('species', params.species);
     }
-    if (name) {
-      params = params.set('name', name);
+    if (params.name) {
+      httpParams = httpParams.set('name', params.name);
     }
 
-    return this.httpClient.get<Animal[]>(`${this.baseUrl}/animals`, { params });
+    return this.httpClient.get<Animal[]>(`${this.baseUrl}/animals`, { params: httpParams });
   }
 
-  public getAnimal(animalId: string) {
+  public getAnimal(animalId: string): Observable<Animal> {
     return this.httpClient.get<Animal>(`${this.baseUrl}/animals/${animalId}`);
   }
+}
+
+export interface SearchAnimalsParams {
+  species?: Species;
+  name?: string;
 }
