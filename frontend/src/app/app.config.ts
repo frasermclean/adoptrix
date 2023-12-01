3ab46b2571bfb0d6f5b5389311ff4432f6b33451
Fraser McLean <dev@frasermclean.com>
2023-12-01 09:26:22 +0800

    Updated app configure to include MSAL components

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,19 +1,47 @@
 import { ApplicationConfig, importProvidersFrom } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
-import { provideRouter, withComponentInputBinding } from '@angular/router';
+import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { provideRouter, withComponentInputBinding, withEnabledBlockingInitialNavigation } from '@angular/router';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import {
+  MSAL_GUARD_CONFIG,
+  MSAL_INSTANCE,
+  MSAL_INTERCEPTOR_CONFIG,
+  MsalBroadcastService,
+  MsalGuard,
+  MsalInterceptor,
+  MsalService,
+} from '@azure/msal-angular';
 import { NgxsModule } from '@ngxs/store';
 
 import { routes } from './app.routes';
 import { environment } from '../environments/environment';
+import { msalGuardConfigurationFactory, msalInstanceFactory, msalInterceptorConfigurationFactory } from './auth.config';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(routes, withComponentInputBinding()),
+    provideRouter(routes, withComponentInputBinding(), withEnabledBlockingInitialNavigation()),
     provideAnimations(),
-    importProvidersFrom([
-      HttpClientModule,
-      NgxsModule.forRoot([], { developmentMode: !environment.isProduction }),
-    ]),
+    provideHttpClient(withInterceptorsFromDi()),
+    importProvidersFrom([NgxsModule.forRoot([], { developmentMode: !environment.isProduction })]),
+    MsalService,
+    MsalGuard,
+    MsalBroadcastService,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: MsalInterceptor,
+      multi: true,
+    },
+    {
+      provide: MSAL_INSTANCE,
+      useFactory: msalInstanceFactory,
+    },
+    {
+      provide: MSAL_GUARD_CONFIG,
+      useFactory: msalGuardConfigurationFactory,
+    },
+    {
+      provide: MSAL_INTERCEPTOR_CONFIG,
+      useFactory: msalInterceptorConfigurationFactory,
+    },
   ],
 };
