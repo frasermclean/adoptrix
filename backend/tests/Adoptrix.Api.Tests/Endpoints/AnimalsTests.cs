7940adaab4646a89abe3ed1338624d1b54242738
Fraser McLean <dev@frasermclean.com>
2023-12-14 10:51:13 +0800

    Improved animals tests

--- backend/tests/Adoptrix.Api.Tests/Endpoints/AnimalsTests.cs
+++ backend/tests/Adoptrix.Api.Tests/Endpoints/AnimalsTests.cs
@@ -30,7 +30,7 @@ public class AnimalsTests(ApiTestFixture fixture, ITestOutputHelper outputHelper
                 .GETAsync<SearchAnimalsEndpoint, SearchAnimalsCommand, IEnumerable<AnimalResponse>>(command);
 
         // assert
-        message.StatusCode.Should().Be(HttpStatusCode.OK);
+        message.Should().HaveStatusCode(HttpStatusCode.OK);
         responses.Should().HaveCount(3).And.AllSatisfy(ValidateAnimalResponse);
     }
 
@@ -49,7 +49,7 @@ public class AnimalsTests(ApiTestFixture fixture, ITestOutputHelper outputHelper
             await httpClient.GETAsync<GetAnimalEndpoint, GetAnimalCommand, AnimalResponse>(command);
 
         // assert
-        message.StatusCode.Should().Be(expectedStatusCode);
+        message.Should().HaveStatusCode(expectedStatusCode);
         if (expectedStatusCode == HttpStatusCode.OK)
         {
             ValidateAnimalResponse(response);
@@ -57,41 +57,37 @@ public class AnimalsTests(ApiTestFixture fixture, ITestOutputHelper outputHelper
     }
 
     [Theory]
-    [InlineData("Fido", "A good boy", "dog", "Labrador", HttpStatusCode.Created)]
-    [InlineData("Rufus", "Another good boy", "dog", MockBreedsRepository.UnknownBreedName, HttpStatusCode.BadRequest)]
-    [InlineData("Max", "", MockSpeciesRepository.UnknownSpeciesName, "Eastern Gray", HttpStatusCode.BadRequest)]
-    public async Task AddAnimal_Should_Return_ExpectedResult(string name, string description, string species,
-        string breed, HttpStatusCode expectedStatusCode)
+    [InlineData("Fido", "A good boy", "dog", "Labrador", Sex.Male, 2, HttpStatusCode.Created)]
+    [InlineData("Rufus", "Another good boy", "dog", MockBreedsRepository.UnknownBreedName)]
+    [InlineData("Max", "", MockSpeciesRepository.UnknownSpeciesName, "Eastern Gray")]
+    [InlineData(null, null, null, null)]
+    public async Task AddAnimal_Should_Return_ExpectedResult(string? name, string? description, string? species,
+        string? breed, Sex? sex = default, int ageInYears = default,
+        HttpStatusCode expectedStatusCode = HttpStatusCode.BadRequest)
     {
         // arrange
-        var command = CreateAddAnimalCommand(name, description, species, breed);
+        var command = new AddAnimalCommand
+        {
+            Name = name!,
+            Description = description,
+            Species = species!,
+            Breed = breed!,
+            Sex = sex,
+            DateOfBirth = DateOnly.FromDateTime(DateTime.Today - TimeSpan.FromDays(365 * ageInYears))
+        };
 
         // act
         var (message, response) =
             await httpClient.POSTAsync<AddAnimalEndpoint, AddAnimalCommand, AnimalResponse>(command);
 
         // assert
-        message.StatusCode.Should().Be(expectedStatusCode);
+        message.Should().HaveStatusCode(expectedStatusCode);
         if (expectedStatusCode == HttpStatusCode.Created)
         {
             message.Headers.Should().ContainKey("Location").WhoseValue.Should().Equal($"api/animals/{response.Id}");
             ValidateAnimalResponse(response);
         }
     }
-
-    [Fact]
-    public async Task AddAnimal_WithInvalidParameters_Should_ReturnBadRequest()
-    {
-        // arrange
-        var command = CreateAddAnimalCommand();
-
-        // act
-        var message = await httpClient.POSTAsync<AddAnimalEndpoint, AddAnimalCommand>(command);
-
-        // assert
-        message.StatusCode.Should().Be(HttpStatusCode.BadRequest);
-    }
-
 
     private static void ValidateAnimalResponse(AnimalResponse response)
     {
@@ -102,15 +98,4 @@ public class AnimalsTests(ApiTestFixture fixture, ITestOutputHelper outputHelper
         response.Sex.Should().NotBeNull();
         response.DateOfBirth.Should().NotBe(default);
     }
-
-    private static AddAnimalCommand CreateAddAnimalCommand(string? name = null, string? description = null,
-        string? species = null, string? breed = null, int ageInYears = 2, Sex sex = Sex.Male) => new()
-    {
-        Name = name!,
-        Description = description,
-        Species = species!,
-        Breed = breed,
-        Sex = sex,
-        DateOfBirth = DateOnly.FromDateTime(DateTime.Today - TimeSpan.FromDays(365 * ageInYears))
-    };
 }
