e3bf3082371887bbea0f70aae2e5e89b67e4f124
Fraser McLean <dev@frasermclean.com>
2023-12-20 13:41:47 +0800

    Enhance image gallery component

--- frontend/src/app/shared/image-gallery/image-gallery.component.ts
+++ frontend/src/app/shared/image-gallery/image-gallery.component.ts
@@ -1,18 +1,28 @@
-import { Component, Input, ViewChild } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { ImageInformation } from '@models/image-information.model';
-import { Gallery, GalleryItem, GalleryModule } from 'ng-gallery';
+import { Gallery, GalleryItem, GalleryModule, ImageItem } from 'ng-gallery';
+import { LightboxModule } from 'ng-gallery/lightbox';
 
 @Component({
   selector: 'app-image-gallery',
   standalone: true,
-  imports: [GalleryModule],
+  imports: [GalleryModule, LightboxModule],
   templateUrl: './image-gallery.component.html',
   styleUrl: './image-gallery.component.scss',
 })
-export class ImageGalleryComponent {
+export class ImageGalleryComponent implements OnInit {
   @Input({ required: true }) images!: ImageInformation[];
 
-  readonly galleryItems: GalleryItem[] = [];
+  readonly galleryId = 'gallery';
+  galleryItems: GalleryItem[] = [];
 
   constructor(private galleryService: Gallery) {}
+
+  ngOnInit(): void {
+    this.galleryItems = this.images.map(
+      (image) => new ImageItem({ src: image.uri, thumb: image.uri, args: { description: image.description } })
+    );
+    const galleryRef = this.galleryService.ref(this.galleryId);
+    galleryRef.load(this.galleryItems);
+  }
 }
