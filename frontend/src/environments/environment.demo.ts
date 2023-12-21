2e97431a4d6b0de4d727d3d6817953f6df40fcd9
Fraser McLean <dev@frasermclean.com>
2023-12-21 14:41:34 +0800

    Update environments to have blob storage base URL

--- frontend/src/environments/environment.demo.ts
+++ frontend/src/environments/environment.demo.ts
@@ -3,6 +3,7 @@ import { Environment } from './environment.interface';
 export const environment: Environment = {
   isDevelopment: false,
   apiBaseUrl: 'https://api.demo.adoptrix.com/api',
+  blobStorageBaseUrl: 'https://adoptrixdemo.blob.core.windows.net/animal-images',
   auth: {
     clientId: 'd100dfc4-d993-4e4a-8ebb-a6a55ef72809',
     redirectUri: 'https://demo.adoptrix.com',
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -3,6 +3,7 @@ import { Environment } from './environment.interface';
 export const environment: Environment = {
   isDevelopment: true,
   apiBaseUrl: 'https://localhost:5443/api',
+  blobStorageBaseUrl: 'http://localhost:10000/devstoreaccount1/animal-images',
   auth: {
     clientId: '91556c30-ad29-4d36-a3df-29701eaba8a9',
     redirectUri: 'http://localhost:4200',
