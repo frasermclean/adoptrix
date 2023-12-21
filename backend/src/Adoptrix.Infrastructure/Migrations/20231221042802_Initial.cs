d08fd8cac73ee0e8b743dacf879b1c0e286d899f
Fraser McLean <dev@frasermclean.com>
2023-12-21 12:36:56 +0800

    Recreate initial migration

---
+++ backend/src/Adoptrix.Infrastructure/Migrations/20231221042802_Initial.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Adoptrix.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Species",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false),
+                    CreatedBy = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2(2)", precision: 2, nullable: false, defaultValueSql: "getutcdate()")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Species", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Breeds",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(30)", maxLength: 30, nullable: false),
+                    SpeciesId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CreatedBy = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2(2)", precision: 2, nullable: false, defaultValueSql: "getutcdate()")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Breeds", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Breeds_Species_SpeciesId",
+                        column: x => x.SpeciesId,
+                        principalTable: "Species",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Animals",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    SpeciesId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    BreedId = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    Sex = table.Column<string>(type: "char(1)", maxLength: 1, nullable: true),
+                    DateOfBirth = table.Column<DateOnly>(type: "date", nullable: false),
+                    CreatedBy = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2(2)", precision: 2, nullable: false, defaultValueSql: "getutcdate()")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Animals", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Animals_Breeds_BreedId",
+                        column: x => x.BreedId,
+                        principalTable: "Breeds",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Animals_Species_SpeciesId",
+                        column: x => x.SpeciesId,
+                        principalTable: "Species",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AnimalImages",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    AnimalId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    FileName = table.Column<string>(type: "varchar(40)", maxLength: 40, nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    OriginalFileName = table.Column<string>(type: "nvarchar(512)", maxLength: 512, nullable: false),
+                    UploadedBy = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    UploadedAt = table.Column<DateTime>(type: "datetime2(2)", precision: 2, nullable: false, defaultValueSql: "getutcdate()")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AnimalImages", x => new { x.AnimalId, x.Id });
+                    table.ForeignKey(
+                        name: "FK_AnimalImages_Animals_AnimalId",
+                        column: x => x.AnimalId,
+                        principalTable: "Animals",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Species",
+                columns: new[] { "Id", "CreatedBy", "Name" },
+                values: new object[,]
+                {
+                    { new Guid("c9c1836b-1051-45c3-a2c4-0d841e69e6d3"), new Guid("00000000-0000-0000-0000-000000000000"), "Cat" },
+                    { new Guid("e6d11a53-bacb-4a8b-a171-beea7e935467"), new Guid("00000000-0000-0000-0000-000000000000"), "Horse" },
+                    { new Guid("f2d44dc6-6c6c-41d0-ad8d-e6c814b09c1a"), new Guid("00000000-0000-0000-0000-000000000000"), "Dog" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Breeds",
+                columns: new[] { "Id", "CreatedBy", "Name", "SpeciesId" },
+                values: new object[,]
+                {
+                    { new Guid("4fb1e168-bf13-4702-9b61-0b8df2ef0c7d"), new Guid("00000000-0000-0000-0000-000000000000"), "Golden Retriever", new Guid("f2d44dc6-6c6c-41d0-ad8d-e6c814b09c1a") },
+                    { new Guid("9b2ace0b-fb18-4da4-86a5-c7404cfbf145"), new Guid("00000000-0000-0000-0000-000000000000"), "German Shepherd", new Guid("f2d44dc6-6c6c-41d0-ad8d-e6c814b09c1a") },
+                    { new Guid("e719fea1-14d5-45a3-845d-404c88c4549f"), new Guid("00000000-0000-0000-0000-000000000000"), "Labrador Retriever", new Guid("f2d44dc6-6c6c-41d0-ad8d-e6c814b09c1a") }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Animals_BreedId",
+                table: "Animals",
+                column: "BreedId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Animals_SpeciesId",
+                table: "Animals",
+                column: "SpeciesId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Breeds_Name",
+                table: "Breeds",
+                column: "Name",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Breeds_SpeciesId",
+                table: "Breeds",
+                column: "SpeciesId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Species_Name",
+                table: "Species",
+                column: "Name",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AnimalImages");
+
+            migrationBuilder.DropTable(
+                name: "Animals");
+
+            migrationBuilder.DropTable(
+                name: "Breeds");
+
+            migrationBuilder.DropTable(
+                name: "Species");
+        }
+    }
+}
