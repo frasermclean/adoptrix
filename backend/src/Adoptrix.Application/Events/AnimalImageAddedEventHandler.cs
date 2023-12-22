ee80e5d393d28886ac56be1020c02d8abe749747
Fraser McLean <dev@frasermclean.com>
2023-12-22 12:07:04 +0800

    Added setting of IsProcessed flag

--- backend/src/Adoptrix.Application/Events/AnimalImageAddedEventHandler.cs
+++ backend/src/Adoptrix.Application/Events/AnimalImageAddedEventHandler.cs
@@ -1,17 +1,25 @@
-﻿using Adoptrix.Application.Services;
+﻿using Adoptrix.Application.Models;
+using Adoptrix.Application.Services;
+using Adoptrix.Application.Services.Repositories;
 using Adoptrix.Domain;
 using FastEndpoints;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace Adoptrix.Application.Events;
 
-public class AnimalImageAddedEventHandler(IServiceScopeFactory serviceScopeFactory, IImageProcessor imageProcessor)
+public class AnimalImageAddedEventHandler(
+    ILogger<AnimalDeletedEventHandler> logger,
+    IServiceScopeFactory serviceScopeFactory,
+    IImageProcessor imageProcessor)
     : IEventHandler<AnimalImageAddedEvent>
 {
     public async Task HandleAsync(AnimalImageAddedEvent eventModel, CancellationToken cancellationToken)
     {
+        // resolve scoped services
         await using var scope = serviceScopeFactory.CreateAsyncScope();
         var animalImageManager = scope.ServiceProvider.GetRequiredService<IAnimalImageManager>();
+        var animalsRepository = scope.ServiceProvider.GetRequiredService<IAnimalsRepository>();
 
         var (animalId, imageId) = eventModel;
 
@@ -22,6 +30,15 @@ public class AnimalImageAddedEventHandler(IServiceScopeFactory serviceScopeFacto
         // process original image
         await using var bundle = await imageProcessor.ProcessOriginalAsync(originalReadStream, cancellationToken);
 
+        await UploadImagesAsync(animalImageManager, animalId, imageId, bundle, cancellationToken);
+
+        // update database
+        await UpdateAnimalAsync(animalsRepository, animalId, imageId, cancellationToken);
+    }
+
+    private async Task UploadImagesAsync(IAnimalImageManager animalImageManager,
+        Guid animalId, Guid imageId, ImageStreamBundle bundle, CancellationToken cancellationToken)
+    {
         // upload processed images
         await Task.WhenAll(
             animalImageManager.UploadImageAsync(animalId, imageId, bundle.ThumbnailWriteStream,
@@ -31,5 +48,25 @@ public class AnimalImageAddedEventHandler(IServiceScopeFactory serviceScopeFacto
             animalImageManager.UploadImageAsync(animalId, imageId, bundle.FullSizeWriteStream,
                 ImageProcessor.OutputContentType, ImageCategory.FullSize, cancellationToken)
         );
+
+        logger.LogInformation("Uploaded processed images for animal with ID: {AnimalId}", animalId);
+    }
+
+    private async Task UpdateAnimalAsync(IAnimalsRepository animalsRepository, Guid animalId, Guid imageId,
+        CancellationToken cancellationToken)
+    {
+        var result = await animalsRepository.GetAsync(animalId, cancellationToken);
+        if (result.IsFailed)
+        {
+            logger.LogError("Failed to get animal with ID: {AnimalId}", animalId);
+            return;
+        }
+
+        var animal = result.Value;
+        var image = animal.Images.First(image => image.Id == imageId);
+        image.IsProcessed = true;
+
+        logger.LogInformation("Set IsProcessed flag for image ID: {ImageId} on animal {AnimalId}", image.Id, animalId);
+        await animalsRepository.UpdateAsync(result.Value, cancellationToken);
     }
 }
