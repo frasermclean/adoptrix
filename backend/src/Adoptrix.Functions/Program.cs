5db7e43d0f215702965a7a826f128ebe3278fef0
Fraser McLean <dev@frasermclean.com>
2023-12-27 14:20:36 +0800

    Create Azure Functions project

---
+++ backend/src/Adoptrix.Functions/Program.cs
@@ -0,0 +1,15 @@
+using Microsoft.Extensions.Hosting;
+
+namespace Adoptrix.Functions;
+
+public static class Program
+{
+    public static void Main()
+    {
+        var host = new HostBuilder()
+            .ConfigureFunctionsWorkerDefaults()
+            .Build();
+
+        host.Run();
+    }
+}
