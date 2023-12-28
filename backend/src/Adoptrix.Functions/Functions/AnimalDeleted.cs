aa1e2e03310a8c3be6a8c986aeca046de4bcea9a
Fraser McLean <dev@frasermclean.com>
2023-12-28 15:04:41 +0800

    Implement delete animal images function

--- backend/src/Adoptrix.Functions/Functions/AnimalDeleted.cs
+++ backend/src/Adoptrix.Functions/Functions/AnimalDeleted.cs
@@ -2,14 +2,23 @@
 using Adoptrix.Domain.Events;
 using Adoptrix.Infrastructure.Storage;
 using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
 
 namespace Adoptrix.Functions.Functions;
 
-public class AnimalDeleted(IAnimalImageManager animalImageManager)
+public class AnimalDeleted(ILogger<AnimalDeleted> logger, IAnimalImageManager animalImageManager)
 {
     [Function(nameof(AnimalDeleted))]
     public async Task Run([QueueTrigger(QueueNames.AnimalDeleted)] AnimalDeletedEvent eventData)
     {
-        await animalImageManager.DeleteAnimalImagesAsync(eventData.AnimalId);
+        var result = await animalImageManager.DeleteAnimalImagesAsync(eventData.AnimalId);
+        if (result.IsFailed)
+        {
+            logger.LogError("Failed to delete animal images for animal {AnimalId}: {Error}",
+                eventData.AnimalId, result.Errors.First().Message);
+            throw new Exception("Failed to delete animal images");
+        }
+
+        logger.LogInformation("Deleted {Count} images for animal {AnimalId}", result.Value, eventData.AnimalId);
     }
 }
