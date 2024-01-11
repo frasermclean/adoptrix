4ad02061cb77b31c31de0194e220ffbdb84e4f14
Fraser McLean <dev@frasermclean.com>
2024-01-11 14:12:16 +0800

    Add test project to Jobs

---
+++ backend/tests/Adoptrix.Jobs.Tests/Extensions/LoggerMockExtensions.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace Adoptrix.Jobs.Tests.Extensions;
+
+public static class LoggerMockExtensions
+{
+    public static void VerifyLog<T>(
+        this Mock<ILogger<T>> loggerMock,
+        string expectedMessage,
+        LogLevel expectedLogLevel = LogLevel.Information,
+        Times? times = null)
+    {
+        times ??= Times.AtLeastOnce();
+
+        loggerMock.Verify(
+            x => x.Log(
+                It.Is<LogLevel>(l => l == expectedLogLevel),
+                It.IsAny<EventId>(),
+                It.Is<It.IsAnyType>((o, t) => CompareMessage(o, t, expectedMessage)),
+                It.IsAny<Exception>(),
+                It.Is<Func<It.IsAnyType, Exception, string>>((v, t) => true)!), (Times)times);
+    }
+
+    private static bool CompareMessage(object? obj, Type _, string expectedMessage)
+    {
+        return obj?.ToString() == expectedMessage;
+    }
+}
