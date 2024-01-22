b9136b537f662d889b066dade0b8d6e02a63c6bb
Fraser McLean <dev@frasermclean.com>
2024-01-22 12:38:33 +0800

    Calculate list of SWA origins

--- infra/Update-AppServiceCors.ps1
+++ infra/Update-AppServiceCors.ps1
@@ -5,7 +5,12 @@ param (
   $AppServiceName = "adoptrix-demo-backend-app"
 )
 
-# Get environment names
-$swaEnvNames = az staticwebapp environment list --name $StaticWebAppName --query "[].name" | ConvertFrom-Json
+# Get the allowed origins from the Static Web App
+$swaCustomDomains = az staticwebapp show --name $StaticWebAppName --query "customDomains" | ConvertFrom-Json
+$swaHostnames = az staticwebapp environment list --name $StaticWebAppName --query "[].hostname" | ConvertFrom-Json
+$swaHostnames += $swaCustomDomains
+$swaOrigins = $swaHostnames | ForEach-Object { "https://$_" }
+
+# Update App Service CORS settings with the allowed origins
 
 Write-Host "Updating CORS settings for $AppServiceName"
