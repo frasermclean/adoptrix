9bb9c24a646bb8e2eafaa683c11bf6b103587a21
Fraser McLean <dev@frasermclean.com>
2024-02-06 08:54:44 +0800

    Add Application Insights telemetry

--- backend/src/Adoptrix.Api/Startup/ServiceRegistration.cs
+++ backend/src/Adoptrix.Api/Startup/ServiceRegistration.cs
@@ -26,6 +26,7 @@ public static class ServiceRegistration
         });
 
         builder.Services
+            .AddApplicationInsightsTelemetry()
             .AddFastEndpoints()
             .AddAuthentication(builder.Configuration)
             .AddValidators()
