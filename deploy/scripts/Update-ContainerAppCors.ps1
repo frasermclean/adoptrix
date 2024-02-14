c05bd798b56dd7393b948bc747e4b20111c8d2d6
Fraser McLean <dev@frasermclean.com>
2024-02-14 14:11:34 +0800

    Add function to flatten array and further enhance script

--- deploy/scripts/Update-ContainerAppCors.ps1
+++ deploy/scripts/Update-ContainerAppCors.ps1
@@ -2,19 +2,31 @@ param (
   [Parameter(Mandatory = $true)][string] $ResourceGroupName
 )
 
+function ConvertTo-FlattenedArray {
+  $input | ForEach-Object {
+    if ($_ -is [array]) {
+      $_ | ConvertTo-FlattenedArray
+    }
+    else {
+      $_
+    }
+  }
+}
+
 function Get-StaticWebAppOrigins([string]$resourceGroup) {
   $app = Get-AzStaticWebApp -ResourceGroupName $resourceGroup
 
   Write-Host "Detected Static Web App: $($app.Name)"
-  $builds = Get-AzStaticWebAppBuild -ResourceGroupName $resourceGroup -Name $app.Name
+  $environmentHostnames = Get-AzStaticWebAppBuild -ResourceGroupName $resourceGroup -Name $app.Name | ForEach-Object { $_.Hostname }
 
   # Calculate origins from default hostname, custom domains and build environment hostnames
-  $origins = @($app.DefaultHostname, $app.CustomDomain, ($builds | ForEach-Object { $_.Hostname }))
+  $origins = @($app.DefaultHostname, $app.CustomDomain, $environmentHostnames)
+  | ConvertTo-FlattenedArray
   | ForEach-Object { "https://$_" }
   | Sort-Object
   | Get-Unique
 
-  Write-Host "Calculated $($origins.Length) origins that should be allowed: $($origins -join ', ')"
+  Write-Host "Calculated $($origins.Length) origins that should be allowed"
 
   return $origins
 }
@@ -23,16 +35,26 @@ function Update-ContainerAppCors([string]$resourceGroup, [string[]]$origins) {
   $app = Get-AzContainerApp -ResourceGroupName $resourceGroup
   Write-Host "Detected container app: $($app.Name)"
 
+  # Add origins that are not yet allowed
   foreach ($origin in $origins) {
     if ($app.Configuration.CorPolicyAllowedOrigin -notcontains $origin) {
       Write-Host "Will add origin: $origin"
-      $app.Configuration.CorPolicyAllowedOrigin += $origin
       $updateRequired = $true
     }
   }
 
+  # Remove origins that are no longer required
+  foreach ($origin in $app.Configuration.CorPolicyAllowedOrigin) {
+    if ($origins -notcontains $origin) {
+      Write-Host "Will remove origin: $origin" -ForegroundColor Yellow
+      $updateRequired = $true
+    }
+  }
+
+  # Update container app if required
   if ($updateRequired) {
-    Write-Host "Updating container app CORS policy"
+    $app.Configuration.CorPolicyAllowedOrigin = $origins
+    Write-Host "Updating container app CORS policy with $($origins.Length) origins."
     Update-AzContainerApp -ResourceGroupName $resourceGroup -Name $app.Name -Configuration $app.Configuration
   }
   else {
