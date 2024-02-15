14785cd764c5887a8c2833ef0334d0aaf5e64f5d
Fraser McLean <dev@frasermclean.com>
2024-02-15 13:22:54 +0800

    Refactor search species endpoint

--- backend/src/Adoptrix.Api/Startup/MiddlewareConfiguration.cs
+++ backend/src/Adoptrix.Api/Startup/MiddlewareConfiguration.cs
@@ -1,4 +1,5 @@
 ﻿using Adoptrix.Api.Endpoints.Animals;
+using Adoptrix.Api.Endpoints.Species;
 using Adoptrix.Api.Endpoints.Users;
 using FastEndpoints;
 
@@ -56,6 +57,9 @@ public static class MiddlewareConfiguration
         adminGroup.MapPost("animals", AddAnimalEndpoint.ExecuteAsync);
         adminGroup.MapDelete("animals/{animalId:guid}", DeleteAnimalEndpoint.ExecuteAsync);
         adminGroup.RequireAuthorization();
+
+        var speciesGroup = apiGroup.MapGroup("species");
+        speciesGroup.MapGet("", SearchSpeciesEndpoint.ExecuteAsync);
 
         var usersGroup = apiGroup.MapGroup("users");
         usersGroup.MapGet("me", GetCurrentUserEndpoint.Execute);
--- backend/tests/Adoptrix.Api.Tests/Endpoints/UsersEndpointTests.cs
+++ backend/tests/Adoptrix.Api.Tests/Endpoints/UsersEndpointTests.cs
@@ -1,5 +1,4 @@
 ﻿using System.Net;
-using System.Net.Http.Headers;
 using System.Net.Http.Json;
 using Adoptrix.Api.Contracts.Responses;
 using Adoptrix.Api.Tests.Fixtures;
