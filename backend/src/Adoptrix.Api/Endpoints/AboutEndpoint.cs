fb7c7e9ec38f62f7888c207e4acaa1fd7c351a26
Fraser McLean <dev@frasermclean.com>
2024-02-19 20:10:57 +0800

    Create about endpoint and response

---
+++ backend/src/Adoptrix.Api/Endpoints/AboutEndpoint.cs
@@ -0,0 +1,12 @@
+﻿using Adoptrix.Api.Contracts.Responses;
+
+namespace Adoptrix.Api.Endpoints;
+
+public static class AboutEndpoint
+{
+    private static readonly AboutResponse Response = new(
+        Version: Environment.GetEnvironmentVariable("APP_VERSION") ?? "Unknown",
+        Environment: Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Unknown");
+
+    public static AboutResponse Execute() => Response;
+}
