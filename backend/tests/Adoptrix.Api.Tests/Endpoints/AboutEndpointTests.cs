21e4e467a7c2a65dbd4d384437f78b83e64bef0e
Fraser McLean <dev@frasermclean.com>
2024-02-20 10:04:07 +0800

    Add simple test for about endpoint

---
+++ backend/tests/Adoptrix.Api.Tests/Endpoints/AboutEndpointTests.cs
@@ -0,0 +1,17 @@
+﻿using Adoptrix.Api.Endpoints;
+
+namespace Adoptrix.Api.Tests.Endpoints;
+
+public class AboutEndpointTests
+{
+    [Fact]
+    public void AboutEndpoint_Should_ReturnVersionAndEnvironment()
+    {
+        // act
+        var response = AboutEndpoint.Execute();
+
+        // assert
+        response.Version.Should().NotBeNullOrEmpty();
+        response.Environment.Should().NotBeNullOrEmpty();
+    }
+}
