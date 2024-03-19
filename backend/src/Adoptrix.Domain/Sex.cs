683ad8b1f9b7cd8de3b759812a5e2fc7363fcc92
Fraser McLean <dev@frasermclean.com>
2024-03-19 12:38:52 +0800

    Add Unknown sex type to backend logic

--- backend/src/Adoptrix.Domain/Sex.cs
+++ backend/src/Adoptrix.Domain/Sex.cs
@@ -2,6 +2,7 @@
 
 public enum Sex
 {
+    Unknown,
     Male,
     Female
 }
--- backend/src/Adoptrix.Infrastructure/ValueConverters/SexConverter.cs
+++ backend/src/Adoptrix.Infrastructure/ValueConverters/SexConverter.cs
@@ -4,22 +4,23 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Adoptrix.Infrastructure.ValueConverters;
 
 public class SexConverter()
-    : ValueConverter<Sex?, char?>(sex => SexToChar(sex), c => CharToSex(c))
+    : ValueConverter<Sex, char>(sex => SexToChar(sex), c => CharToSex(c))
 {
+    internal const char UnknownChar = 'X';
     private const char MaleChar = 'M';
     private const char FemaleChar = 'F';
 
-    private static char? SexToChar(Sex? sex) => sex switch
+    private static char SexToChar(Sex sex) => sex switch
     {
         Sex.Male => MaleChar,
         Sex.Female => FemaleChar,
-        _ => null
+        _ => UnknownChar
     };
 
-    private static Sex? CharToSex(char? c) => c switch
+    private static Sex CharToSex(char c) => c switch
     {
         MaleChar => Sex.Male,
         FemaleChar => Sex.Female,
-        _ => null
+        _ => Sex.Unknown
     };
 }
