e200bc9393ee68fac3901b3a637ce549b0bc5f82
Fraser McLean <dev@frasermclean.com>
2024-03-23 10:38:08 +0800

    Add tests for species get by name method

--- backend/tests/Adoptrix.Infrastructure.Tests/Services/Repositories/SpeciesRepositoryTests.cs
+++ backend/tests/Adoptrix.Infrastructure.Tests/Services/Repositories/SpeciesRepositoryTests.cs
@@ -21,4 +21,18 @@ public class SpeciesRepositoryTests(DatabaseFixture fixture) : IClassFixture<Dat
         result.Should().BeSuccess();
         result.Value.Name.Should().Be(expectedName);
     }
+
+    [Theory]
+    [InlineData("Cat", "c9c1836b-1051-45c3-a2c4-0d841e69e6d3")]
+    [InlineData("Dog", "f2d44dc6-6c6c-41d0-ad8d-e6c814b09c1a")]
+    [InlineData("Horse", "e6d11a53-bacb-4a8b-a171-beea7e935467")]
+    public async Task GetByNameAsync_WithValidName_Should_Return_ExpectedResult(string name, Guid expectedId)
+    {
+        // act
+        var result = await repository.GetByNameAsync(name);
+
+        // assert
+        result.Should().BeSuccess();
+        result.Value.Id.Should().Be(expectedId);
+    }
 }
