8b4f32a5817f4d758317744d4940ffb36fa78970
Fraser McLean <dev@frasermclean.com>
2024-04-12 13:04:06 +0800

    Add tests for update animal command handler

--- backend/src/Adoptrix.Application/Features/Animals/Commands/UpdateAnimalCommandHandler.cs
+++ backend/src/Adoptrix.Application/Features/Animals/Commands/UpdateAnimalCommandHandler.cs
@@ -3,10 +3,14 @@ using Adoptrix.Application.Services;
 using Adoptrix.Domain.Models;
 using FluentResults;
 using MediatR;
+using Microsoft.Extensions.Logging;
 
 namespace Adoptrix.Application.Features.Animals.Commands;
 
-public class UpdateAnimalCommandHandler(IAnimalsRepository animalsRepository, IBreedsRepository breedsRepository)
+public class UpdateAnimalCommandHandler(
+    ILogger<UpdateAnimalCommandHandler> logger,
+    IAnimalsRepository animalsRepository,
+    IBreedsRepository breedsRepository)
     : IRequestHandler<UpdateAnimalCommand, Result<Animal>>
 {
     public async Task<Result<Animal>> Handle(UpdateAnimalCommand command, CancellationToken cancellationToken)
@@ -14,12 +18,14 @@ public class UpdateAnimalCommandHandler(IAnimalsRepository animalsRepository, IB
         var animal = await animalsRepository.GetByIdAsync(command.AnimalId, cancellationToken);
         if (animal is null)
         {
+            logger.LogError("Animal with ID {AnimalId} was not found", command.AnimalId);
             return new AnimalNotFoundError(command.AnimalId);
         }
 
         var breed = await breedsRepository.GetByIdAsync(command.BreedId, cancellationToken);
         if (breed is null)
         {
+            logger.LogError("Breed with ID {BreedId} was not found", command.BreedId);
             return new BreedNotFoundError(command.BreedId);
         }
 
@@ -30,6 +36,9 @@ public class UpdateAnimalCommandHandler(IAnimalsRepository animalsRepository, IB
         animal.DateOfBirth = command.DateOfBirth;
 
         await animalsRepository.UpdateAsync(animal, cancellationToken);
+
+        logger.LogInformation("Updated animal with ID: {AnimalId}", animal.Id);
+
         return animal;
     }
 }
