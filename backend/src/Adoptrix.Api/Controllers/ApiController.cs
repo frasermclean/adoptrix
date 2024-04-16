a6e1901491232d7bac20b74ce3a74b19766087f2
Fraser McLean <dev@frasermclean.com>
2024-04-16 17:46:05 +0800

    Include validation error details when there is a validation problem

--- backend/src/Adoptrix.Api/Controllers/ApiController.cs
+++ backend/src/Adoptrix.Api/Controllers/ApiController.cs
@@ -2,6 +2,7 @@
 using FluentResults;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 
 namespace Adoptrix.Api.Controllers;
 
@@ -10,17 +11,38 @@ namespace Adoptrix.Api.Controllers;
 [Route("api/[controller]")]
 public abstract class ApiController : ControllerBase
 {
-    protected ObjectResult Problem(IEnumerable<IError> errors)
+    protected IActionResult Problem(IList<IError> errors)
     {
+        if (errors.Any(error => error is ValidationError))
+        {
+            return ValidationProblem(errors.OfType<ValidationError>());
+        }
+
         var (statusCode, detail) = errors.First() switch
         {
-            ValidationError => (StatusCodes.Status400BadRequest, "A validation error occurred"),
             INotFoundError notFoundError => (StatusCodes.Status404NotFound, notFoundError.Message),
-            _ => (StatusCodes.Status500InternalServerError, "An unhandled error occurred")
+            _ => (StatusCodes.Status500InternalServerError, null)
         };
 
         return Problem(
             detail: detail,
             statusCode: statusCode);
+    }
+
+    private ActionResult ValidationProblem(IEnumerable<ValidationError> errors)
+    {
+        var dictionary = new ModelStateDictionary();
+
+        foreach (var error in errors)
+        {
+            if (error.Metadata[ValidationError.PropertyNameKey] is not string key)
+            {
+                continue;
+            }
+
+            dictionary.AddModelError(key, error.Message);
+        }
+
+        return ValidationProblem(dictionary);
     }
 }
