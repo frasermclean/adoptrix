83d4f696e9d76a18aeddde8309ebef7b14564ef6
Fraser McLean <dev@frasermclean.com>
2024-04-22 18:33:13 +0800

    Add dynamic chips with species names

--- frontend/src/app/animals/animals-list/search-controls/search-controls.component.ts
+++ frontend/src/app/animals/animals-list/search-controls/search-controls.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
+import { MatChipsModule } from '@angular/material/chips';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
+import { Store } from '@ngxs/store';
+import { SpeciesActions } from '@state/species.actions';
+import { SpeciesState } from '@state/species.state';
 
 @Component({
   selector: 'app-search-controls',
@@ -13,6 +17,7 @@ import { MatInputModule } from '@angular/material/input';
     CommonModule,
     FormsModule,
     MatButtonModule,
+    MatChipsModule,
     MatFormFieldModule,
     MatIconModule,
     MatInputModule,
@@ -20,6 +25,16 @@ import { MatInputModule } from '@angular/material/input';
   templateUrl: './search-controls.component.html',
   styleUrl: './search-controls.component.scss',
 })
-export class SearchControlsComponent {
+export class SearchControlsComponent implements OnInit {
+  speciesMatches$ = this.store.select(SpeciesState.matches);
+
+  constructor(private store: Store) {}
+  ngOnInit(): void {
+    this.store.dispatch(new SpeciesActions.Search({ withAnimals: true }));
+  }
   value = '';
+
+  onSpeciesChanged(speciesId: string): void {
+    console.log(speciesId);
+  }
 }
