7268d4f86d55b251207dbdd9e837a5c174adea7f
Fraser McLean <dev@frasermclean.com>
2024-07-10 14:43:24 +0800

    Add tests for search animals endpoint

--- backend/tests/Adoptrix.Tests/Mocks/AnimalsRepositoryMockSetup.cs
+++ backend/tests/Adoptrix.Tests/Mocks/AnimalsRepositoryMockSetup.cs
@@ -1,6 +1,4 @@
 ﻿using Adoptrix.Application.Services.Abstractions;
-using Adoptrix.Core.Contracts.Requests.Animals;
-using Adoptrix.Core.Contracts.Responses;
 using Adoptrix.Tests.Shared.Factories;
 
 namespace Adoptrix.Tests.Mocks;
@@ -11,25 +9,6 @@ public static class AnimalsRepositoryMockSetup
 
     public static Mock<IAnimalsRepository> SetupDefaults(this Mock<IAnimalsRepository> mock, int searchResultsCount = 3)
     {
-        mock.Setup(repository =>
-                repository.SearchAsync(It.IsAny<SearchAnimalsRequest>(), It.IsAny<CancellationToken>()))
-            .ReturnsAsync((SearchAnimalsRequest _, CancellationToken _) => AnimalFactory.CreateMany(searchResultsCount)
-                .Select(animal => new AnimalMatch
-                {
-                    Id = animal.Id,
-                    Name = animal.Name,
-                    SpeciesName = animal.Breed.Species.Name,
-                    BreedName = animal.Breed.Name,
-                    Sex = animal.Sex,
-                    DateOfBirth = animal.DateOfBirth,
-                    CreatedAt = animal.CreatedAt,
-                    Image = animal.Images.Select(image => new AnimalImageResponse
-                        {
-                            Id = image.Id, Description = image.Description, IsProcessed = image.IsProcessed
-                        })
-                        .FirstOrDefault(),
-                }));
-
         mock.Setup(repository => repository.GetByIdAsync(It.IsAny<Guid>(), It.IsAny<CancellationToken>()))
             .ReturnsAsync((Guid animalId, CancellationToken _) => animalId == UnknownAnimalId
                 ? null
