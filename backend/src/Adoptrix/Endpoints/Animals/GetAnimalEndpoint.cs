5543cdfcb92dc6f8365e20f98fb7f8812dfdbfd5
Fraser McLean <dev@frasermclean.com>
2024-07-11 15:46:25 +0800

    Improve animal responses to include images

--- backend/src/Adoptrix/Endpoints/Animals/GetAnimalEndpoint.cs
+++ backend/src/Adoptrix/Endpoints/Animals/GetAnimalEndpoint.cs
@@ -1,6 +1,8 @@
 ﻿using Adoptrix.Core.Abstractions;
 using Adoptrix.Core.Contracts.Responses;
+using Adoptrix.Extensions;
 using Adoptrix.Mapping;
+using Adoptrix.Storage;
 using FastEndpoints;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http.HttpResults;
@@ -8,7 +10,10 @@ using Microsoft.AspNetCore.Http.HttpResults;
 namespace Adoptrix.Endpoints.Animals;
 
 [HttpGet("animals/{animalId:guid}"), AllowAnonymous]
-public class GetAnimalEndpoint(IAnimalsRepository animalsRepository)
+public class GetAnimalEndpoint(
+    IAnimalsRepository animalsRepository,
+    [FromKeyedServices(BlobContainerNames.AnimalImages)]
+    IBlobContainerManager blobContainerManager)
     : Endpoint<GetAnimalRequest, Results<Ok<AnimalResponse>, NotFound>>
 {
     public override async Task<Results<Ok<AnimalResponse>, NotFound>> ExecuteAsync(GetAnimalRequest request,
@@ -20,6 +25,12 @@ public class GetAnimalEndpoint(IAnimalsRepository animalsRepository)
             return TypedResults.NotFound();
         }
 
-        return TypedResults.Ok(animal.ToResponse());
+        var response = animal.ToResponse();
+        foreach (var image in response.Images)
+        {
+            image.SetImageUrls(animal.Id, blobContainerManager.ContainerUri);
+        }
+
+        return TypedResults.Ok(response);
     }
 }
--- backend/src/Adoptrix/Endpoints/Animals/SearchAnimalsEndpoint.cs
+++ backend/src/Adoptrix/Endpoints/Animals/SearchAnimalsEndpoint.cs
@@ -1,18 +1,29 @@
 ﻿using Adoptrix.Core.Abstractions;
 using Adoptrix.Core.Contracts.Requests.Animals;
 using Adoptrix.Core.Contracts.Responses;
+using Adoptrix.Extensions;
+using Adoptrix.Storage;
 using FastEndpoints;
 using Microsoft.AspNetCore.Authorization;
 
 namespace Adoptrix.Endpoints.Animals;
 
 [HttpGet("animals"), AllowAnonymous]
-public class SearchAnimalsEndpoint(IAnimalsRepository animalsRepository) : Endpoint<SearchAnimalsRequest, IEnumerable<AnimalMatch>>
+public class SearchAnimalsEndpoint(
+    IAnimalsRepository animalsRepository,
+    [FromKeyedServices(BlobContainerNames.AnimalImages)] IBlobContainerManager blobContainerManager)
+    : Endpoint<SearchAnimalsRequest, IEnumerable<AnimalMatch>>
 {
     public override async Task<IEnumerable<AnimalMatch>> ExecuteAsync(SearchAnimalsRequest request,
         CancellationToken cancellationToken)
     {
         var matches = await animalsRepository.SearchAsync(request, cancellationToken);
+
+        foreach (var match in matches)
+        {
+            match.Image?.SetImageUrls(match.Id, blobContainerManager.ContainerUri);
+        }
+
         return matches;
     }
 }
