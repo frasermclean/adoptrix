56e95c305c52473ea4da41c9ccb5f5a98c392709
Fraser McLean <dev@frasermclean.com>
2024-07-12 12:17:01 +0800

    Add tests for ProcessAnimalImage function

--- tests/Adoptrix.Jobs.Tests/Functions/ProcessAnimalImageTests.cs
+++ tests/Adoptrix.Jobs.Tests/Functions/ProcessAnimalImageTests.cs
@@ -9,14 +9,16 @@ namespace Adoptrix.Jobs.Tests.Functions;
 
 public class ProcessAnimalImageTests
 {
-    [Theory(Skip = "Not currently working"), AdoptrixAutoData]
-    public async Task ExecuteAsync_WithValidEventData_ShouldPass(ProcessAnimalImage sut, Animal animal,
+    [Theory, AdoptrixAutoData]
+    public async Task ExecuteAsync_WithValidEventData_ShouldPass(Animal animal, ImageStreamBundle bundle,
         [Frozen] Mock<IAnimalsRepository> animalsRepositoryMock,
+        [Frozen] Mock<IBlobContainerManager> blobContainerManagerMock,
         [Frozen] Mock<IImageProcessor> imageProcessorMock,
-        ImageStreamBundle bundle)
+        ProcessAnimalImage function)
     {
         // arrange
-        var data = new AnimalImageAddedEvent(animal.Id, animal.Images.First().Id);
+        var imageId = animal.Images.First().Id;
+        var data = new AnimalImageAddedEvent(animal.Id, imageId);
         animalsRepositoryMock.Setup(repository => repository.GetByIdAsync(data.AnimalId, It.IsAny<CancellationToken>()))
             .ReturnsAsync(animal);
         imageProcessorMock.Setup(processor =>
@@ -24,6 +26,36 @@ public class ProcessAnimalImageTests
             .ReturnsAsync(bundle);
 
         // act
-        await sut.ExecuteAsync(data);
+        await function.ExecuteAsync(data);
+
+        // assert
+        animalsRepositoryMock.Verify(repository => repository.GetByIdAsync(animal.Id, It.IsAny<CancellationToken>()),
+            Times.Once);
+        blobContainerManagerMock.Verify(manager =>
+            manager.OpenReadStreamAsync(It.IsAny<string>(), It.IsAny<CancellationToken>()), Times.Once);
+        imageProcessorMock.Verify(processor => processor.ProcessOriginalAsync(It.IsAny<Stream>(),
+            It.IsAny<CancellationToken>()), Times.Once);
+        blobContainerManagerMock.Verify(manager =>
+            manager.UploadBlobAsync(It.IsAny<string>(), It.IsAny<Stream>(), It.IsAny<string>(),
+                It.IsAny<CancellationToken>()), Times.Exactly(3));
+        animalsRepositoryMock.Verify(repository => repository.SaveChangesAsync(It.IsAny<CancellationToken>()),
+            Times.Once);
+    }
+
+    [Theory, AdoptrixAutoData]
+    public async Task ExecuteAsync_WithInvalidAnimalId_ShouldThrowException(AnimalImageAddedEvent data,
+        [Frozen] Mock<IAnimalsRepository> animalsRepositoryMock, ProcessAnimalImage function)
+    {
+        // arrange
+        animalsRepositoryMock.Setup(repository => repository.GetByIdAsync(data.AnimalId, It.IsAny<CancellationToken>()))
+            .ReturnsAsync(null as Animal);
+
+        // act
+        var act = async () => await function.ExecuteAsync(data);
+
+        // assert
+        await act.Should().ThrowAsync<InvalidOperationException>();
+        animalsRepositoryMock.Verify(repository => repository.GetByIdAsync(data.AnimalId, It.IsAny<CancellationToken>()),
+            Times.Once);
     }
 }
