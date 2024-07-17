893051981f0c283fa485f0f0580a39ae9e090881
Fraser McLean <dev@frasermclean.com>
2024-07-17 13:17:29 +0800

    Adjust Aspire and Docker Compose to use the same data volume names

--- src/Adoptrix.AppHost/Program.cs
+++ src/Adoptrix.AppHost/Program.cs
@@ -1,3 +1,4 @@
+using Aspire.Hosting.Azure;
 using Microsoft.Extensions.Hosting;
 
 namespace Adoptrix.AppHost;
@@ -6,24 +7,9 @@ public static class Program
 {
     public static void Main(string[] args)
     {
-        var builder = DistributedApplication.CreateBuilder(args);
-
-        var sqlServerPassword = builder.AddParameter("sql-server-password");
-
-        var database = builder.AddSqlServer("sql-server", sqlServerPassword)
-            .WithBindMount("../../deps/scripts", "/usr/config")
-            .WithDataVolume("adoptrix-sql-server-data")
-            .WithEntrypoint("/usr/config/entrypoint.sh")
-            .AddDatabase("database", "adoptrix");
-
-        var storage = builder.AddAzureStorage("storage");
-        if (builder.Environment.IsDevelopment())
-        {
-            storage.RunAsEmulator(resourceBuilder => resourceBuilder.WithImageTag("latest"));
-        }
-
-        var blobStorage = storage.AddBlobs("blob-storage");
-        var queueStorage = storage.AddQueues("queue-storage");
+        var builder = DistributedApplication.CreateBuilder(args)
+            .AddSqlServerWithDatabase(out var database)
+            .AddAzureStorage(out var blobStorage, out var queueStorage);
 
         var api = builder.AddProject<Projects.Adoptrix_Api>("adoptrix-api")
             .WithReference(database)
@@ -38,5 +24,38 @@ public static class Program
             .WithReference(api);
 
         builder.Build().Run();
+    }
+
+    private static IDistributedApplicationBuilder AddSqlServerWithDatabase(
+        this IDistributedApplicationBuilder builder,
+        out IResourceBuilder<SqlServerDatabaseResource> database)
+    {
+        var sqlServerPassword = builder.AddParameter("sql-server-password");
+
+        database = builder.AddSqlServer("sql-server", sqlServerPassword)
+            .WithBindMount("../../deps/scripts", "/usr/config")
+            .WithDataVolume("adoptrix-sql-server-data")
+            .WithEntrypoint("/usr/config/entrypoint.sh")
+            .AddDatabase("database", "adoptrix");
+
+        return builder;
+    }
+
+    private static IDistributedApplicationBuilder AddAzureStorage(
+        this IDistributedApplicationBuilder builder,
+        out IResourceBuilder<AzureBlobStorageResource> blobStorage,
+        out IResourceBuilder<AzureQueueStorageResource> queueStorage)
+    {
+        var storage = builder.AddAzureStorage("storage")
+            .RunAsEmulator(resourceBuilder =>
+            {
+                resourceBuilder.WithDataVolume("adoptrix-storage-data");
+                resourceBuilder.WithImageTag("latest");
+            });
+
+        blobStorage = storage.AddBlobs("blob-storage");
+        queueStorage = storage.AddQueues("queue-storage");
+
+        return builder;
     }
 }
