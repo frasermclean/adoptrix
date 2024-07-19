ea2890159521594fded6cc5cbea17907121126db
Fraser McLean <dev@frasermclean.com>
2024-07-19 15:32:03 +0800

    Change Aspire and Docker Compose to use different volumes

--- src/Adoptrix.AppHost/Program.cs
+++ src/Adoptrix.AppHost/Program.cs
@@ -35,10 +35,7 @@ public static class Program
         this IDistributedApplicationBuilder builder,
         out IResourceBuilder<SqlServerDatabaseResource> database)
     {
-        var sqlServerPassword = builder.AddParameter("sql-server-password");
-
-        database = builder.AddSqlServer("sql-server", sqlServerPassword)
-            .WithDataVolume("adoptrix-sql-server-data")
+        database = builder.AddSqlServer("sql-server")
             .AddDatabase("database", "adoptrix");
 
         return builder;
@@ -54,11 +51,7 @@ public static class Program
         // run as emulator in local development
         if (builder.ExecutionContext.IsRunMode)
         {
-            storage.RunAsEmulator(resourceBuilder =>
-            {
-                resourceBuilder.WithDataVolume("adoptrix-storage-data");
-                resourceBuilder.WithImageTag("latest");
-            });
+            storage.RunAsEmulator(resourceBuilder => resourceBuilder.WithImageTag("latest"));
         }
 
         blobStorage = storage.AddBlobs("blob-storage");
