9c52c23f35a88362b946ad77f880a38bff1bd547
Fraser McLean <dev@frasermclean.com>
2024-07-23 15:11:47 +0800

    Upgrade to MudBlazor 7.3.0 and implement breaking changes

--- src/Adoptrix.Web/Extensions/SearchAnimalsRequestExtensions.cs
+++ src/Adoptrix.Web/Extensions/SearchAnimalsRequestExtensions.cs
@@ -16,22 +16,22 @@ public static class SearchAnimalsRequestExtensions
 
         if (request.BreedId.HasValue)
         {
-            builder.Add("breedId", request.BreedId.ToString());
+            builder.Add("breedId", request.BreedId.ToString()!);
         }
 
         if (request.SpeciesId.HasValue)
         {
-            builder.Add("speciesId", request.SpeciesId.ToString());
+            builder.Add("speciesId", request.SpeciesId.ToString()!);
         }
 
         if (request.Sex.HasValue)
         {
-            builder.Add("sex", request.Sex.ToString());
+            builder.Add("sex", request.Sex.ToString()!);
         }
 
         if (request.Limit.HasValue)
         {
-            builder.Add("limit", request.Limit.ToString());
+            builder.Add("limit", request.Limit.ToString()!);
         }
 
         return builder.ToString();
