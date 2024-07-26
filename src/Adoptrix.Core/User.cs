681d849c59c5f5df1bf2a9323fa37e2ff69b8eea
Fraser McLean <dev@frasermclean.com>
2024-07-26 11:58:41 +0800

    Add user entity with configuration

---
+++ src/Adoptrix.Core/User.cs
@@ -0,0 +1,8 @@
+﻿namespace Adoptrix.Core;
+
+public class User
+{
+    public Guid Id { get; init; }
+    public UserRole Role { get; init; }
+    public DateTime CreatedAt { get; init; }
+}
--- src/Adoptrix.Persistence/Services/AdoptrixDbContext.cs
+++ src/Adoptrix.Persistence/Services/AdoptrixDbContext.cs
@@ -9,11 +9,13 @@ public class AdoptrixDbContext(DbContextOptions<AdoptrixDbContext> options) : Db
     public DbSet<Species> Species => Set<Species>();
     public DbSet<Breed> Breeds => Set<Breed>();
     public DbSet<Animal> Animals => Set<Animal>();
+    public DbSet<User> Users => Set<User>();
 
     protected override void OnModelCreating(ModelBuilder builder)
     {
         builder.ApplyConfiguration(new AnimalConfiguration());
         builder.ApplyConfiguration(new BreedConfiguration());
         builder.ApplyConfiguration(new SpeciesConfiguration());
+        builder.ApplyConfiguration(new UserConfiguration());
     }
 }
