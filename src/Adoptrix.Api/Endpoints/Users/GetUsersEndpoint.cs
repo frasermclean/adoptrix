604f311d5cf3fb5c4648b34da7b2089123324b3f
Fraser McLean <dev@frasermclean.com>
2024-07-28 10:05:04 +0800

    Add get users endpoint

---
+++ src/Adoptrix.Api/Endpoints/Users/GetUsersEndpoint.cs
@@ -0,0 +1,32 @@
+﻿using Adoptrix.Contracts.Responses;
+using FastEndpoints;
+using Microsoft.Graph;
+
+namespace Adoptrix.Api.Endpoints.Users;
+
+[HttpGet("users")]
+public class GetUsersEndpoint(GraphServiceClient graphServiceClient) : EndpointWithoutRequest<IEnumerable<UserResponse>>
+{
+    public override async Task<IEnumerable<UserResponse>> ExecuteAsync(CancellationToken cancellationToken)
+    {
+        var response = await graphServiceClient.Users.GetAsync(configuration =>
+        {
+            configuration.QueryParameters.Select = ["id", "givenName", "surname", "displayName", "mail"];
+            configuration.QueryParameters.Orderby = ["displayName"];
+        }, cancellationToken);
+
+        if (response?.Value is null)
+        {
+            return [];
+        }
+
+        return response.Value.Select(user => new UserResponse
+        {
+            Id = user.Id ?? string.Empty,
+            FirstName = user.GivenName,
+            LastName = user.Surname,
+            DisplayName = user.DisplayName,
+            EmailAddress = user.Mail
+        });
+    }
+}
---
+++ src/Adoptrix.Contracts/Responses/UserResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace Adoptrix.Contracts.Responses;
+
+public class UserResponse
+{
+    public required string Id { get; init; }
+    public string? FirstName { get; init; }
+    public string? LastName { get; init; }
+    public string? DisplayName { get; init; }
+    public string? EmailAddress { get; init; }
+}
