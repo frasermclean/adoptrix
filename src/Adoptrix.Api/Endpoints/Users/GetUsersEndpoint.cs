28de92f6032a48191c23a53b702cd28b1d8cb174
Fraser McLean <dev@frasermclean.com>
2024-07-28 13:25:48 +0800

    Enhance get users endpoint response to include roles from database

--- src/Adoptrix.Api/Endpoints/Users/GetUsersEndpoint.cs
+++ src/Adoptrix.Api/Endpoints/Users/GetUsersEndpoint.cs
@@ -1,13 +1,46 @@
-﻿using Adoptrix.Contracts.Responses;
+﻿using Adoptrix.Api.Extensions;
+using Adoptrix.Contracts.Responses;
+using Adoptrix.Persistence.Services;
 using FastEndpoints;
 using Microsoft.Graph;
+using Microsoft.Graph.Models;
+using Microsoft.Identity.Web;
 
 namespace Adoptrix.Api.Endpoints.Users;
 
 [HttpGet("users")]
-public class GetUsersEndpoint(GraphServiceClient graphServiceClient) : EndpointWithoutRequest<IEnumerable<UserResponse>>
+public class GetUsersEndpoint(GraphServiceClient graphServiceClient, IUsersRepository usersRepository)
+    : EndpointWithoutRequest<IEnumerable<UserResponse>>
 {
     public override async Task<IEnumerable<UserResponse>> ExecuteAsync(CancellationToken cancellationToken)
+    {
+        var graphUsersTask = GetGraphUsersAsync(cancellationToken);
+        var repositoryUsersTask = usersRepository.GetAllAsync(cancellationToken);
+
+        await Task.WhenAll(graphUsersTask, repositoryUsersTask);
+
+        if (graphUsersTask.Result?.Value is null)
+        {
+            return [];
+        }
+
+        return graphUsersTask.Result.Value.Select(user =>
+        {
+            var userId = Guid.Parse(user.Id!);
+            return new UserResponse
+            {
+                Id = userId,
+                FirstName = user.GivenName,
+                LastName = user.Surname,
+                DisplayName = user.DisplayName,
+                EmailAddress = user.Mail,
+                Role = repositoryUsersTask.Result.FirstOrDefault(u => u.Id == userId)?.Role.ToString(),
+                IsCurrentUser = User.GetUserId() == userId
+            };
+        }).Where(response => response.Role is not null);
+    }
+
+    private async Task<UserCollectionResponse?> GetGraphUsersAsync(CancellationToken cancellationToken)
     {
         var response = await graphServiceClient.Users.GetAsync(configuration =>
         {
@@ -15,18 +48,6 @@ public class GetUsersEndpoint(GraphServiceClient graphServiceClient) : EndpointW
             configuration.QueryParameters.Orderby = ["displayName"];
         }, cancellationToken);
 
-        if (response?.Value is null)
-        {
-            return [];
-        }
-
-        return response.Value.Select(user => new UserResponse
-        {
-            Id = user.Id ?? string.Empty,
-            FirstName = user.GivenName,
-            LastName = user.Surname,
-            DisplayName = user.DisplayName,
-            EmailAddress = user.Mail
-        });
+        return response;
     }
 }
--- src/Adoptrix.Contracts/Responses/UserResponse.cs
+++ src/Adoptrix.Contracts/Responses/UserResponse.cs
@@ -2,9 +2,11 @@
 
 public class UserResponse
 {
-    public required string Id { get; init; }
+    public Guid Id { get; init; }
     public string? FirstName { get; init; }
     public string? LastName { get; init; }
     public string? DisplayName { get; init; }
     public string? EmailAddress { get; init; }
+    public string? Role { get; init; }
+    public bool IsCurrentUser { get; init; }
 }
