72bd58a23f6e3ce45276037c0040ae069566384b
Fraser McLean <dev@frasermclean.com>
2024-10-03 14:16:22 +0800

    Add get current user endpoint

---
+++ tests/Adoptrix.Api.Tests/Endpoints/Users/GetCurrentUserEndpointTests.cs
@@ -0,0 +1,25 @@
+﻿using System.Net;
+using Adoptrix.Api.Tests.Fixtures;
+using Adoptrix.Core;
+using Adoptrix.Core.Responses;
+
+namespace Adoptrix.Api.Tests.Endpoints.Users;
+
+public class GetCurrentUserEndpointTests(MockServicesFixture fixture) : TestBase<MockServicesFixture>
+{
+    private readonly HttpClient httpClient = fixture.CreateClient(UserRole.User);
+
+    [Fact]
+    public async Task GetCurrentUser_WithValidRequest_ShouldReturnOk()
+    {
+        // arrange
+        fixture.UserManagerMock.Setup(manager => manager.GetUserAsync(It.IsAny<Guid>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync(new UserResponse());
+
+        // act
+        var message = await httpClient.GetAsync("api/users/me");
+
+        // assert
+        message.StatusCode.Should().Be(HttpStatusCode.OK);
+    }
+}
