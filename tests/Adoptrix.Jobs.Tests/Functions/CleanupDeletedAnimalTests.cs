e49f8ab37c6d5474aa83df9b0f14f1ae2645a377
Fraser McLean <dev@frasermclean.com>
2024-10-06 19:14:23 +0800

    Restore test for CleanupDeletedAnimal function

--- tests/Adoptrix.Jobs.Tests/Functions/CleanupDeletedAnimalTests.cs
+++ tests/Adoptrix.Jobs.Tests/Functions/CleanupDeletedAnimalTests.cs
@@ -1,5 +1,29 @@
-﻿namespace Adoptrix.Jobs.Tests.Functions;
+﻿using Adoptrix.Core.Events;
+using Adoptrix.Jobs.Functions;
+using Adoptrix.Persistence.Services;
+using Adoptrix.Tests.Shared;
+
+namespace Adoptrix.Jobs.Tests.Functions;
 
 public class CleanupDeletedAnimalTests
 {
+    [Theory, AdoptrixAutoData]
+    public async Task ExecuteAsync_WithValidEventData_ShouldPass(AnimalDeletedEvent eventData,
+        [Frozen] Mock<IBlobContainerManager> animalImagesManagerMock,
+        CleanupDeletedAnimal function)
+    {
+        // arrange
+        var blobNames = new[] { "image1.jpg", "image2.jpg", "image3.jpg" };
+        animalImagesManagerMock
+            .Setup(manager => manager.GetBlobNamesAsync(It.IsAny<string>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync(blobNames);
+
+        // act
+        await function.ExecuteAsync(eventData);
+
+        // assert
+        animalImagesManagerMock.Verify(
+            manager => manager.DeleteBlobAsync(It.IsAny<string>(), It.IsAny<CancellationToken>()),
+            Times.Exactly(blobNames.Length));
+    }
 }
