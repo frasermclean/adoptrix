a317d3506aa66aa2a97d18ab5b6ef05d9d599471
Fraser McLean <dev@frasermclean.com>
2024-10-08 14:03:56 +0800

    Update demo environment application insights connection string

--- frontend/src/environments/environment.demo.ts
+++ frontend/src/environments/environment.demo.ts
@@ -11,6 +11,6 @@ export const environment: Environment = {
   },
   appInsights: {
     connectionString:
-      'InstrumentationKey=c1f91173-56d4-4237-a79f-04f5d08e2af5;IngestionEndpoint=https://australiaeast-1.in.applicationinsights.azure.com/;LiveEndpoint=https://australiaeast.livediagnostics.monitor.azure.com/;ApplicationId=b180f887-d8c3-491d-8660-c9f59163789b',
+      'InstrumentationKey=f31d8a50-134b-44f2-b121-7261052e2f90;IngestionEndpoint=https://australiaeast-1.in.applicationinsights.azure.com/;LiveEndpoint=https://australiaeast.livediagnostics.monitor.azure.com/;ApplicationId=5bb5ae25-c5ae-47f6-99eb-67c131c63a4c',
   },
 };
